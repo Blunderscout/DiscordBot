ec45247f13beab48f6faf5b10820de91702bc951
Spencer Beck <spencerb936@gmail.com>
2021-12-12 19:34:58 -0500

    use class

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# bot.py
+import discord
 import os
-import random
-from dotenv import load_dotenv
 
-from discord.ext import commands
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
 
-bot = commands.Bot(command_prefix='!')
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-
-bot.run(TOKEN)
+client = MyClient()
+client.run(os.environ.get("DISCORD_TOKEN"))
